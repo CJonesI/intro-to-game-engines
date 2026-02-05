2987b267da6d0055fc18912599746589e9dae611
CJonesI <cjones@usv.edu>
2026-02-04 23:05:33 -0800

    JonesCharjani_Mechanics_02

--- Scripts/slime.gd
+++ Scripts/slime.gd
@@ -1,6 +1,23 @@
 extends Node2D
 
+const SPEED = 30
+
+var direction = 1
+@onready var ray_cast_left = $Left
+@onready var ray_cast_right = $right
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	position.x +1
+	if ray_cast_right.is_colliding():
+		direction = 2
+	if ray_cast_left.is_colliding():
+		direction = -2
+	
+		
+	position.x += direction * SPEED * delta
+
+func _on_hit_area_body_entered(body: Node2D):
+	if body.name =="Player":
+		body.get_node("CollisionShape2D").queue_free()
+		queue_free()
+		
