324a5d26c32518638f272bbbba8bc7fd89c0705f
CJonesI <cjones@usv.edu>
2026-02-18 14:01:39 -0800

    post update

--- Scripts/killzone.gd
+++ Scripts/killzone.gd
@@ -3,10 +3,11 @@ extends Area2D
 @onready var timer = $Timer
 
 func _on_body_entered(body):
-	print("You Died")
-	Engine.time_scale = 0.5
-	body.get_node("CollisionShape2D").queue_free()
-	timer.start()
+	if body.name=="Player":
+		print("You Died")
+		Engine.time_scale = 0.5
+	 
+		timer.start()
 	
 func _on_timer_timeout():
 	Engine.time_scale = 1.0
--- Scripts/slime.gd
+++ Scripts/slime.gd
@@ -6,6 +6,5 @@ var direction = 1
 
 func _on_hit_area_body_entered(body: Node2D):
 	if body.name =="Player":
-		body.get_node("CollisionShape2D").queue_free()
 		queue_free()
 		
